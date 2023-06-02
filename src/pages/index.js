38a75a94bc578a95b91ef145a004b0eccd89ed1a
Jacob <jacobcohen@duck.com>
2023-06-02 01:07:17 -0500

    Mobile Style Updates
    Reworked CSS on the site as a whole to make it fully work on mobile now. Some other small fixes and additions. Still not completely satisfied with projects landing page so might work on that some more

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <Layout title={`Home`}>
       <div className="centered">
-        <img className='circleImage' src={iconPicture} alt="My Icon" style={{ maxHeight: 500, maxWidth: 500, }} />
+        <img className='circleImage' src={iconPicture} alt="My Icon"/>
         <h1 className="hero__title">{"Hey there name's Jacob"}</h1>
         <h2>Welcome to my little corner of the internet</h2>
         <div className='buttonCenter'>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import useBaseUrl from "@docusaurus/useBaseUrl";
-import Link from '@docusaurus/Link';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
 
 //pictures
 import rulesPicture from '@site/static/projectImages/theRulesHaveChanged.jpg'
@@ -12,29 +17,30 @@ import personalWebsite from '@site/static/projectImages/personalWebsite.png'
 import valorantWebsite from '@site/static/projectImages/valorantWebsite.png'
 import ghubScript from '@site/static/projectImages/ghubScript.png'
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+//css
+import styles from '@site/src/css/projects.module.css'
+
 
 export default function Projects() {
     return (
         <Layout title='Projects'>
             <h1 className='header'>My Projects</h1>
-            <Grid container direction={"row"} justifyContent={"center"} rowSpacing={5} columnSpacing={{ xs: 1, sm: 20, md: 50}}>
+            <Grid container direction={"row"} justifyContent={"center"} rowSpacing={5} columnSpacing={{ xs: 1, sm: 20, md: 50 }}>
                 {/*Black ops 3 mod*/}
                 <Grid>
                     <Card sx={{ width: 345 }}>
-                        <Link to={useBaseUrl("projects/changeTheRules")}>
+                        <CardActionArea href={useBaseUrl("projects/changeTheRules")} className={styles.projectCard}>
                             <CardMedia
                                 sx={{ height: 300 }}
                                 image={rulesPicture}
                                 alt="Change the Rules Logo"
                             />
-                            <CardContent className='projectCard'>
+                            <CardContent className={styles.projectCardText}>
                                 <h1>Change The Rules</h1>
                                 <p>A gameplay mod for BlackOps</p>
+                                <h3 className={styles.projectCardButton}>More Info</h3>
                             </CardContent>
-                        </Link>
+                        </CardActionArea>
                     </Card>
 
                 </Grid>
@@ -42,85 +48,90 @@ export default function Projects() {
                 {/*thejewbot*/}
                 <Grid>
                     <Card sx={{ width: 345 }}>
-                        <Link to={useBaseUrl("projects/discordBot")}>
+                        <CardActionArea href={useBaseUrl("projects/discordBot")} className={styles.projectCard}>
                             <CardMedia
                                 sx={{ height: 300 }}
                                 image={discordBot}
                                 alt="My Discord bot's logo"
                             />
-                            <CardContent className='projectCard'>
+                            <CardContent className={styles.projectCardText}>
                                 <h1>Discord Bot</h1>
                                 <p>A personal Discord bot I made</p>
+                                <h3 className={styles.projectCardButton}>More Info</h3>
                             </CardContent>
-                        </Link>
+                        </CardActionArea>
                     </Card>
                 </Grid>
 
                 {/*bot website*/}
                 <Grid>
                     <Card sx={{ width: 345 }}>
-                        <Link to={useBaseUrl("projects/botWebsite")}>
+                        <CardActionArea href={useBaseUrl("projects/botWebsite")} className={styles.projectCard}>
                             <CardMedia
                                 sx={{ height: 300 }}
                                 image={botWebsite}
                                 alt="My Discord Bot Logo"
                             />
-                            <CardContent className='projectCard'>
+                            <CardContent className={styles.projectCardText}>
                                 <h1>Bot Website</h1>
                                 <p>A website that I made for my Discord Bot.</p>
+                                <h3 className={styles.projectCardButton}>More Info</h3>
                             </CardContent>
-                        </Link>
+                        </CardActionArea>
                     </Card>
                 </Grid>
 
                 {/*this website*/}
                 <Grid>
                     <Card sx={{ width: 345 }}>
-                        <Link to={useBaseUrl("projects/personalWebsite")}>
+                        <CardActionArea href={useBaseUrl("projects/personalWebsite")} className={styles.projectCard}>
                             <CardMedia
                                 sx={{ height: 300 }}
                                 image={personalWebsite}
                                 alt="Personal Website Picture"
                             />
-                            <CardContent className='projectCard'>
+                            <CardContent className={styles.projectCardText}>
                                 <h1>My Personal Website</h1>
                                 <p>My personal website that I made to show my personal projects that I have worked on.</p>
+                                <h3 className={styles.projectCardButton}>More Info</h3>
                             </CardContent>
-                        </Link>
+                        </CardActionArea>
                     </Card>
                 </Grid>
 
                 {/*Valorant Strat Site*/}
                 <Grid>
                     <Card sx={{ width: 345 }}>
-                        <Link to={useBaseUrl("projects/valorantStratSite")}>
+                        <CardActionArea href={useBaseUrl("projects/valorantStratSite")} className={styles.projectCard}>
                             <CardMedia
                                 sx={{ height: 300 }}
                                 image={valorantWebsite}
                                 alt="Valorant Strat Roulette Website Picture"
                             />
-                            <CardContent className='projectCard'>
+                            <CardContent className={styles.projectCardText}>
                                 <h1>Valorant Strat Roulette Website</h1>
                                 <p>A simple strat roulette website for the game Valorant.</p>
+                                <h3 className={styles.projectCardButton}>More Info</h3>
                             </CardContent>
-                        </Link>
+                        </CardActionArea>
                     </Card>
                 </Grid>
 
                 {/*Logitech gHub profile adder*/}
                 <Grid>
                     <Card sx={{ width: 345 }}>
-                        <Link to={useBaseUrl("projects/ghubGamePass")}>
+                        <CardActionArea href={useBaseUrl("projects/ghubGamePass")} className={styles.projectCard}>
                             <CardMedia
                                 sx={{ height: 300 }}
                                 image={ghubScript}
                                 alt="Logitech ghub Script Image"
                             />
-                            <CardContent className='projectCard'>
+                            <CardContent className={styles.projectCardText}>
                                 <h1>gHub Gamepass Profile Creator</h1>
                                 <p>A script that helps add Xbox Gamepass games to Logitech gHub.</p>
+                                <h3 className={styles.projectCardButton}>More Info</h3>
                             </CardContent>
-                        </Link>
+                        </CardActionArea>
                     </Card>
                 </Grid>
 
